5c67936b699efcd977bb7e4cde5059e68e26da15
LuisEduardoDev1 <luisedunole@gmail.com>
2024-10-09 14:58:08 -0300

    first commit

---
+++ app/Http/Controllers/CadastrosController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Salas;
+use Illuminate\Http\Request;
+
+class CadastrosController extends Controller
+{
+    //
+
+    public function cadastroSala(Request $request){
+        $register = new Salas;
+
+        $register->numero = $request->campoNumero;
+        $register->quantidade = $request->campoQtd;
+
+        $register->save();
+        return redirect()->route('inicio')->with('success', 'Sala cadastrada com sucesso!');
+    }
+
+    public function index()
+    {
+        $salas = Salas::all();
+        return view('salasDisp', compact('salas'));
+    }
+
+    // public function cadastroDatas(){}
+}
