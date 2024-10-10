e88b981bf781b63357530b7879428b5f77f1259b
Luis Eduardo <134558250+LuisEduardoDev1@users.noreply.github.com>
2024-10-10 12:45:16 -0300

    Update 2024_08_21_124400_create_usuarios_table.php

--- database/migrations/2024_08_21_124400_create_usuarios_table.php
+++ database/migrations/2024_08_21_124400_create_usuarios_table.php
@@ -80,6 +80,15 @@ return new class extends Migration
             $table->date('created_at')->nullable();
             $table->date('updated_at')->nullable();
         });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     public function down()
