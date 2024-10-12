87fc481f5513e78c1e1a9b56252a2eefdd16bdc7
Luis Eduardo <134558250+LuisEduardoDev1@users.noreply.github.com>
2024-10-12 17:10:55 -0300

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer install --no-dev --working-dir=/var/www/html
+#--no-dev --working-dir=/var/www/html
+composer install 
 composer update
 
 echo "Caching config..."
