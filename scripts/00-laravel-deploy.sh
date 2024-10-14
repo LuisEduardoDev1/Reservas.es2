42212c1590d6add8513dfc7e8c0b7f0294e1c70f
Luis Eduardo <134558250+LuisEduardoDev1@users.noreply.github.com>
2024-10-13 21:04:23 -0300

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,7 +11,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force 
+php artisan migrate:fresh --force 
 
 echo "Publishing cloudinary provider..."
 php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
