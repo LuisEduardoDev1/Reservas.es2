0123a0ebfc58691477ddb45a628aec15a89d18a6
LuisEduardoDev1 <luisedunole@gmail.com>
2024-11-14 11:21:55 -0300

    Teste de correção da pro-reitoria

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,7 +11,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate:refresh --force 
+php artisan migrate --force 
 
 echo "Publishing cloudinary provider..."
 php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
